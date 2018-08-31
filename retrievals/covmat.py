a2e86c12cf96f498876d8e2661cdbd2c36b1f618
Jonas Hagen <jonas.hagen@iap.unibe.ch>
2018-08-31 11:44:06 +0200

    Covmat sparse.

--- retrievals/covmat.py
+++ retrievals/covmat.py
@@ -1,4 +1,26 @@
 import numpy as np
+from scipy import sparse
+
+
+def corr_fun(fname):
+    def f_exp(x):
+        return np.exp(-x)
+
+    def f_lin(x):
+        return 1.0 - (1.0 - np.exp(-1.0)) * x
+
+    def f_gauss(x):
+        return np.exp(-x ** 2)
+
+    if fname == 'lin':
+        return f_lin
+    elif fname == 'exp':
+        return f_exp
+    elif fname == 'gauss':
+        return f_gauss
+    else:
+        raise NotImplementedError(
+            f'Correlation type {fname} is not implemented, only "exp" and "lin" and "gauss" are valid.')
 
 
 def covmat_1d(grid1, sigma1, cl1, grid2=None, sigma2=None, cl2=None, fname='lin', cutoff=0):
@@ -35,35 +57,60 @@ def covmat_1d(grid1, sigma1, cl1, grid2=None, sigma2=None, cl2=None, fname='lin'
         sigma2 = sigma1
         cl2 = cl1
 
-    def f_exp(x):
-        return np.exp(-x)
+    f = corr_fun(fname)
 
-    def f_lin(x):
-        return 1.0 - (1.0 - np.exp(-1.0)) * x
+    x2, x1 = np.meshgrid(grid2, grid1)
+    dist = np.abs(x2 - x1)
 
-    def f_gau(x):
-        return np.exp(-x ** 2)
+    x2, x1 = np.meshgrid(cl2, cl1)
+    cl = (x2 + x1) / 2
 
-    if fname == 'lin':
-        f = f_lin
-    elif fname == 'exp':
-        f = f_exp
-    elif fname == 'gauss':
-        f = f_gau
-    else:
-        raise NotImplementedError(f'Correlation type {fname} is not implemented, only "exp" and "lin" are valid.')
-
-    x1, x2 = np.meshgrid(grid1, grid2)
-    dist = np.abs(x1 - x2)
-
-    x1, x2 = np.meshgrid(cl1, cl2)
-    cl = (x1 + x2) / 2
-
-    var = sigma1[:, np.newaxis].dot(sigma2[np.newaxis, :])
-
+    var = sigma1[:, np.newaxis] @ sigma2[np.newaxis, :]
     S = var * f(dist / cl)
 
     if cutoff > 0:
         S[S < cutoff] = 0
+
+    return S
+
+
+def covmat_1d_sparse(grid1, sigma1, cl1, grid2=None, sigma2=None, cl2=None, fname='lin', cutoff=0):
+    """
+    Same as `covmat_1d` but creates and returns a sparse matrix.
+    :param grid1:
+    :param sigma1:
+    :param cl1:
+    :param grid2:
+    :param sigma2:
+    :param cl2:
+    :param fname:
+    :param cutoff:
+    :return:
+    """
+    if grid2 is None:
+        grid2 = grid1
+        sigma2 = sigma1
+        cl2 = cl1
+        
+    f = corr_fun(fname)
+
+    n1 = len(grid1)
+    n2 = len(grid2)
+    row_ind = []
+    col_ind = []
+    values = []
+
+    for i in range(n1):
+        for j in range(n2):
+            d = np.abs(grid1[i] - grid2[j])
+            c = (cl1[i] + cl2[j]) / 2
+            s = sigma1[i] * sigma2[j]
+            value = s * f(d/c)
+            if value >= cutoff:
+                row_ind.append(i)
+                col_ind.append(j)
+                values.append(value)
+
+    S = sparse.coo_matrix((values, (row_ind, col_ind)), shape=(n1, n2))
 
     return S
--- tests/test_covmat.py
+++ tests/test_covmat.py
@@ -3,7 +3,6 @@ import numpy as np
 from retrievals import covmat
 
 
-@pytest.mark.current
 def test_covmat_1d():
     reference = np.array(
         [0.090000, 0.054588, 0.033109, 0.020082, 0.012180, 0.007388, 0.004481, 0.002718, 0.000000, 0.000000, 0.054588,
@@ -23,3 +22,44 @@ def test_covmat_1d():
     cm = covmat.covmat_1d(x, sigma, cl, fname='exp', cutoff=0.02)
 
     assert np.allclose(cm, reference, atol=0.02)
+
+
+def test_covmat_1d_2():
+    reference = np.array([[0.09, 0.05458776, 0.03310915, 0.02008171, 0.],
+                          [0.05458776, 0.09, 0.05458776, 0.03310915, 0.02008171],
+                          [0.03310915, 0.05458776, 0.09, 0.05458776, 0.03310915],
+                          [0.02008171, 0.03310915, 0.05458776, 0.09, 0.05458776],
+                          [0., 0.02008171, 0.03310915, 0.05458776, 0.09]])
+
+    x1 = np.arange(5)
+    sigma1 = 0.3 * np.ones_like(x1)
+    cl1 = 2 * np.ones_like(x1)
+    cm = covmat.covmat_1d(x1, sigma1, cl1, fname='exp', cutoff=0.02)
+
+    assert np.allclose(cm, reference, atol=0.01)
+
+
+def test_covmat_1d_sparse():
+    x1 = np.arange(5)
+    sigma1 = 0.3 * np.ones_like(x1)
+    cl1 = 2 * np.ones_like(x1)
+
+    S1 = covmat.covmat_1d(x1, sigma1, cl1, fname='lin', cutoff=0.02)
+    S2 = covmat.covmat_1d_sparse(x1, sigma1, cl1, fname='lin', cutoff=0.02)
+
+    assert np.allclose(S1, S2.toarray())
+
+
+@pytest.mark.current
+def test_covmat_1d_sparse_2():
+    x1 = np.arange(5)
+    sigma1 = 0.3 * np.ones_like(x1)
+    cl1 = 2 * np.ones_like(x1)
+    x2 = np.arange(10)
+    sigma2 = 0.3 * np.ones_like(x2)
+    cl2 = 2 * np.ones_like(x2)
+
+    S1 = covmat.covmat_1d(x1, sigma1, cl1, x2, sigma2, cl2, fname='lin', cutoff=0.02)
+    S2 = covmat.covmat_1d_sparse(x1, sigma1, cl1, x2, sigma2, cl2, fname='lin', cutoff=0.02)
+
+    assert np.allclose(S1, S2.toarray())
